ed8b61588505dc3fa5121fe139f6a2102959132d
Edwin Dominguez <edwindominguez@hotmail.com>
2019-11-16 19:14:13 -0500

    add pizza form class, add pizza form and note in html

--- pizza/views.py
+++ pizza/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .forms import PizzaForm
 
 
 def home(request):
@@ -6,4 +7,14 @@ def home(request):
 
 
 def order(request):
-    return render(request, 'pizza/order.html')
+    if request.method == 'POST':
+        filled_form = PizzaForm(request.POST)
+        if filled_form.is_valid():
+            note = 'Thanks for ordering! Your %s %s and %s pizza its on its way' %(filled_form.cleaned_data['size'],
+                                                                                   filled_form.cleaned_data['topping1'],
+                                                                                   filled_form.cleaned_data['topping2'])
+            new_form = PizzaForm()
+            return render(request, 'pizza/order.html', {'pizzaform': new_form, 'note': note})
+    else:
+        form = PizzaForm()
+        return render(request, 'pizza/order.html', {'pizzaform': form})
